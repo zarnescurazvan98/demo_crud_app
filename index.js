bb08a68da8da70950b9b5b426639b65df7418bd7
Razvan Zarnescu <zarnescurazvan98@gmail.com>
2021-11-19 12:08:51 +0200

    Added changes

--- index.js
+++ index.js
@@ -145,3 +145,12 @@ document.addEventListener("DOMContentLoaded", () => {
 			})
 	}
 });
+
+function myFunction() {
+	var x = document.getElementById("show_container");
+	if (x.style.display === "none") {
+	  x.style.display = "block";
+	} else {
+	  x.style.display = "none";
+	}
+  }
